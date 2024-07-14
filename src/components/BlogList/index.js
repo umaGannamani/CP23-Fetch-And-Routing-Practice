a002982eead2558b57b2e511364323287306e663
uma <udayarekhachowdary96@gmail.com>
2024-07-14 10:34:36 +0000

    # CP23-Fetch-And-Routing-Practice

---
+++ src/components/BlogList/index.js
@@ -0,0 +1,53 @@
+// Write your JS code here
+import {Component} from 'react'
+
+import Loader from 'react-loader-spinner'
+
+import 'react-loader-spinner/dist/loader/css/react-spinner-loader.css'
+
+import BlogItem from '../BlogItem'
+
+import './index.css'
+
+class BlogList extends Component {
+  state = {isLodding: true, blogsData: []}
+
+  componentDidMount() {
+    this.getBlogsData()
+  }
+
+  getBlogsData = async () => {
+    const response = await fetch('https://apis.ccbp.in/blogs')
+    const data = await response.json()
+    const formattedData = data.map(eachItem => ({
+      id: eachItem.id,
+      title: eachItem.title,
+      imageUrl: eachItem.image_url,
+      avatarUrl: eachItem.avatar_url,
+      author: eachItem.author,
+      topic: eachItem.topic,
+    }))
+    this.setState({blogsData: formattedData, isLodding: false})
+  }
+
+  render() {
+    const {blogsData, isLodding} = this.state
+    return (
+      <div className="blogs-list-container">
+        {isLodding ? (
+          <div data-testid="loader">
+            <Loader type="TailSpin" color="#00bfff" height={50} width={50} />
+          </div>
+        ) : (
+          <ul className="blogs-list">
+            {blogsData.map(eachItem => (
+              <BlogItem key={eachItem.id} blogItemDetails={eachItem} />
+            ))}
+          </ul>
+        )}
+      </div>
+    )
+  }
+}
+
+export default BlogList
